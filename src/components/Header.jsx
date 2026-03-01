c43ca51889dfc8eab61cf036ef7664bdfffedbef
Gayoung-Han <230981709+Gayoung-Han@users.noreply.github.com>
2026-03-01 13:48:39 +0100

    styled the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
 import { Link } from 'react-router-dom';
+import styles from "./Header.module.css"; // CSS Module 불러오기
 
 export default function Header() {
     return (
-        <header>
-             <nav>
-                <p>
-                    <Link to="/">Home Page</Link>
+        <header className={styles.header}> {/* header 태그에 styles.header 적용 */}
+             <nav className={styles.navbar}> {/* nav 태그에 styles.navbar 적용 */}
+                <p className={styles.navItem}> {/* 각 p 태그에 styles.navItem 적용 */}
+                    <Link to="/" className={styles.navLink}>Home Page</Link> {/* Link 태그에 styles.navLink 적용 */}
                 </p>
-                <p>
-                    <Link to="/firstPage">First Page</Link>
+                <p className={styles.navItem}>
+                    <Link to="/firstPage" className={styles.navLink}>First Page</Link>
                 </p>
-                  <p>
-                    <Link to="/secondPage">Second Page</Link>
+                  <p className={styles.navItem}>
+                    <Link to="/secondPage" className={styles.navLink}>Second Page</Link>
                 </p>
             </nav>
         </header>
